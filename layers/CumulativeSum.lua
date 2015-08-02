1dd4214d8514e23d81cfeb6ef3c36bbe79a5c67d
Prajit Ramachandran <prajit@siftscience.com>
2015-08-02 11:10:33 -0700

    Cumulative sum for batches (sums along second dimension).

--- layers/CumulativeSum.lua
+++ layers/CumulativeSum.lua
@@ -3,28 +3,31 @@ local CumulativeSum, parent = torch.class('nn.CumulativeSum', 'nn.Module')
 function CumulativeSum:__init(reverse)
     parent.__init(self)
     self.reverse = reverse
+    self.input_sum = torch.Tensor()
 end
 
-function reverseCumsum(input)
-    local cumsum = torch.cumsum(input, 1)
-    cumsum:mul(-1):add(torch.sum(input)):add(input)
+function CumulativeSum:reverseCumsum(internal, input)
+    internal:resizeAs(input):cumsum(input, 2)
+    self.input_sum:resizeAs(input)
+    self.input_sum:set(torch.sum(input, 2):repeatTensor(1, input:size(2)))
+    internal:mul(-1):add(self.input_sum):add(input)
     return cumsum
 end
 
 function CumulativeSum:updateOutput(input)
     if not self.reverse then
-       self.output = torch.cumsum(input, 1)
+       self.output:resizeAs(input):cumsum(input, 2)
     else
-       self.output = reverseCumsum(input)
+       self:reverseCumsum(self.output, input)
     end
     return self.output
 end
 
 function CumulativeSum:updateGradInput(input, gradOutput)
     if self.reverse then
-        self.gradInput = torch.cumsum(gradOutput)
+        self.gradInput:resizeAs(gradOutput):cumsum(gradOutput, 2)
     else
-        self.gradInput =  reverseCumsum(gradOutput)
+        self:reverseCumsum(self.gradInput, gradOutput)
     end
     return self.gradInput
 end
--- test/CumulativeSumTest.lua
+++ test/CumulativeSumTest.lua
@@ -6,31 +6,35 @@ local tests = {}
 local jac = nn.Jacobian
 local precision = 1e-6
 
-local cumSum = nn.CumulativeSum(false)
-local reverseCumSum = nn.CumulativeSum(true)
-local x = torch.Tensor{1, 2, 3, 4}
+local cumsum = nn.CumulativeSum(false)
+local reverse_cumsum = nn.CumulativeSum(true)
+local x = torch.Tensor{{1, 2, 3, 4}, {5, 6, 7, 8}}
 
 function tests.testForward()
-    local xCumSum = torch.Tensor{1, 3, 6, 10}
-    local xPredictedCumSum = cumSum:forward(x)
-    tester:assertTensorEq(xCumSum, xPredictedCumSum, precision, 
+    local expected = torch.Tensor{{1, 3, 6, 10}, {5, 11, 18, 26}}
+    local predicted = cumsum:forward(x)
+    tester:assertTensorEq(expected, predicted, precision, 
                           "The cumulative sum is wrong!")
 end
 
 function tests.testReverseForward()
-    local xReverseCumSum = torch.Tensor{10, 9, 7, 4}
-    local xPredictedReverseCumSum = reverseCumSum:forward(x)
-    tester:assertTensorEq(xReverseCumSum, xPredictedReverseCumSum, precision, 
+    local expected = torch.Tensor{{10, 9, 7, 4}, {26, 21, 15, 8}}
+    local predicted = reverse_cumsum:forward(x)
+    tester:assertTensorEq(expected, predicted, precision, 
                           "The reverse cumulative sum is wrong!")
 end
 
 function tests.testBackwards()
-    local err = jac.testJacobian(cumSum, x)
+    local err = jac.testJacobian(cumsum, torch.rand(4, 2))
+    tester:assertlt(err, precision, "The backwards pass is wrong!")
+    err = jac.testJacobian(cumsum, torch.rand(4, 8))
     tester:assertlt(err, precision, "The backwards pass is wrong!")
 end
 
 function tests.testReverseBackwards()
-    local err = jac.testJacobian(reverseCumSum, x)
+    local err = jac.testJacobian(reverse_cumsum, torch.rand(4, 2))
+    tester:assertlt(err, precision, "The reverse backwards pass is wrong!")
+    err = jac.testJacobian(reverse_cumsum, torch.rand(4, 8))
     tester:assertlt(err, precision, "The reverse backwards pass is wrong!")
 end
 
