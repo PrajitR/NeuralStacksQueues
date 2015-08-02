fcf4368f3e394f86225cc4476dd4db48faf9e1a8
Prajit Ramachandran <prajit@siftscience.com>
2015-08-02 11:13:17 -0700

    ScalarAddTable for batches. Scalar is now an nx1 column and vector is nxm matrix.

--- layers/ScalarAddTable.lua
+++ layers/ScalarAddTable.lua
@@ -6,18 +6,13 @@ function ScalarAddTable:__init()
 end
 
 function ScalarAddTable:updateOutput(input)
-    local vector, scalar = unpack(input)
-    self.output:set(vector + scalar[1])
+    local vectors, scalars = unpack(input)
+    self.output:repeatTensor(scalars, 1, vectors:size(2)):add(vectors)
     return self.output
 end
 
 function ScalarAddTable:updateGradInput(input, gradOutput)
-    local vector, scalar = unpack(input)
-    self.gradInput[1] = self.gradInput[1] or input[1].new()
-    self.gradInput[2] = self.gradInput[2] or input[2].new()
-    self.gradInput[2]:resizeAs(scalar)
-
     self.gradInput[1]:set(gradOutput)
-    self.gradInput[2][1] = torch.sum(gradOutput)
+    self.gradInput[2]:sum(gradOutput, 2)
     return self.gradInput
 end
--- test/ScalarAddTableTest.lua
+++ test/ScalarAddTableTest.lua
@@ -8,44 +8,44 @@ local precision = 1e-6
 local add = nn.ScalarAddTable()
 
 function tests.testForward()
-    local x = torch.Tensor{1, 2, 3}
-    local y = torch.Tensor{5}
+    local x = torch.Tensor{{1, 2, 3}, {4, 5, 6}}
+    local y = torch.Tensor{{5}, {-7}}
 
-    local xPlusY = torch.Tensor{6, 7, 8}
+    local expected = torch.Tensor{{6, 7, 8}, {-3, -2, -1}}
     local predicted = add:forward({x, y})
-    tester:assertTensorEq(xPlusY, predicted, precision,
+    tester:assertTensorEq(expected, predicted, precision,
                           "The forward pass is wrong!")
 end
 
 function tests.testBackwards() 
-    local x = torch.rand(5)
-    local y = torch.rand(1)
+    local x = torch.rand(1, 5)
+    local y = torch.rand(1, 1)
     local eps = 1e-6
     local z = add:forward({x, y}):clone()
-    local jac = torch.DoubleTensor(z:size(1), x:size(1) + 1)
+    local jac = torch.DoubleTensor(z:size(2), x:size(2) + 1)
     
-    local one_hot = torch.zeros(z:size())
-    for i = 1, z:size(1) do
-        one_hot[i] = 1
+    local one_hot = torch.zeros(1, z:size(2))
+    for i = 1, z:size(2) do
+        one_hot[{1, i}] = 1
         local back = add:backward({x, y}, one_hot)
         jac[i] = torch.cat(back[1], back[2])
-        one_hot[i] = 0
+        one_hot[{1, i}] = 0
     end
 
-    local jac_est = torch.DoubleTensor(z:size(1), x:size(1) + 1)
-    for i = 1, x:size(1) do
-        x[i] = x[i] + eps
+    local jac_est = torch.DoubleTensor(z:size(2), x:size(2) + 1)
+    for i = 1, x:size(2) do
+        x[{1, i}] = x[{1, i}] + eps
         jac_est[{{}, i}]:copy(add:forward({x, y}))
-        x[i] = x[i] - 2 * eps
+        x[{1, i}] = x[{1, i}] - 2 * eps
         jac_est[{{}, i}]:add(-1, add:forward({x, y})):div(2 * eps)
-        x[i] = x[i] + eps
+        x[{1, i}] = x[{1, i}] + eps
     end
     
-    y[1] = y[1] + eps
+    y[{1, 1}] = y[{1, 1}] + eps
     jac_est[{{}, -1}]:copy(add:forward({x, y}))
-    y[1] = y[1] - 2 * eps
+    y[{1, 1}] = y[{1, 1}] - 2 * eps
     jac_est[{{}, -1}]:add(-1, add:forward({x, y})):div(2 * eps)
-    y[1] = y[1] + eps
+    y[{1, 1}] = y[{1, 1}] + eps
 
     tester:assertTensorEq(jac, jac_est, precision, 
                           "The backwards pass is wrong!")
