7d03754e48208fea35bb686a1476d30305c2463e
Prajit Ramachandran <ramprajit@gmail.com>
2015-09-05 16:39:34 -0500

    Update controller test to test sequence.

--- test/controllerTest.lua
+++ test/controllerTest.lua
@@ -1,5 +1,8 @@
 local Controller = require '../controller'
 
+local tester = torch.Tester()
+local tests = {}
+
 local opt = { 
     dropout = 0.0,
     num_lstm_layers = 2,
@@ -11,78 +14,99 @@ local opt = {
     memory_types = "sqs" }
 local n_memory_vectors = 3
 
-local rnn = Controller.oneSidedMemory(opt)
+function tests.testOneSidedMemory()
+    local rnn1 = Controller.oneSidedMemory(opt)
+    local rnn2 = Controller.oneSidedMemory(opt)
 
-local inputs = {}
-local x = torch.rand(opt.batch_size):mul(opt.vocab_size):add(1):floor()
-table.insert(inputs, x)
-for i = 1, opt.num_lstm_layers do
-    table.insert(inputs, torch.rand(opt.batch_size, opt.rnn_size))
-    table.insert(inputs, torch.rand(opt.batch_size, opt.rnn_size))
-end
-for i = 1, opt.num_memory_modules do
-    table.insert(inputs, torch.rand(opt.batch_size, n_memory_vectors, opt.memory_size))
-    table.insert(inputs, torch.rand(opt.batch_size, n_memory_vectors))
-    table.insert(inputs, torch.rand(opt.batch_size, opt.memory_size))
-end
-print('\nForwards pass!')
-print(unpack(rnn:forward(inputs)))
+    local inputs1 = {}
+    local x = torch.rand(opt.batch_size):mul(opt.vocab_size):add(1):floor()
+    table.insert(inputs1, x)
+    for i = 1, opt.num_lstm_layers do
+        table.insert(inputs1, torch.rand(opt.batch_size, opt.rnn_size))
+        table.insert(inputs1, torch.rand(opt.batch_size, opt.rnn_size))
+    end
+    for i = 1, opt.num_memory_modules do
+        table.insert(inputs1, torch.rand(opt.batch_size, n_memory_vectors, opt.memory_size))
+        table.insert(inputs1, torch.rand(opt.batch_size, n_memory_vectors))
+        table.insert(inputs1, torch.rand(opt.batch_size, opt.memory_size))
+    end
+    local output = rnn1:forward(inputs1)
 
-local grads = {}
-for i = 1, opt.num_lstm_layers do
-    table.insert(grads, torch.rand(opt.batch_size, opt.rnn_size))
-    table.insert(grads, torch.rand(opt.batch_size, opt.rnn_size))
-end
-for i = 1, opt.num_memory_modules do
-    table.insert(grads, torch.rand(opt.batch_size, n_memory_vectors + 1, opt.memory_size))
-    table.insert(grads, torch.rand(opt.batch_size, n_memory_vectors + 1))
-    table.insert(grads, torch.rand(opt.batch_size, opt.memory_size))
-end
-table.insert(grads, torch.rand(opt.batch_size, opt.vocab_size))
+    local inputs2 = {}
+    table.insert(inputs2, x)
+    for i = 1, #output - 1 do
+        table.insert(inputs2, output[i])
+    end
+    rnn2:forward(inputs2)
 
-print('\n\nBackwards pass!')
-local gradient = rnn:backward(inputs, grads)
-print(unpack(gradient))
-rnn:updateParameters(1)
-print(#inputs)
-print(#gradient)
+    local grads = {}
+    for i = 1, opt.num_lstm_layers do
+        table.insert(grads, torch.rand(opt.batch_size, opt.rnn_size))
+        table.insert(grads, torch.rand(opt.batch_size, opt.rnn_size))
+    end
+    for i = 1, opt.num_memory_modules do
+        table.insert(grads, torch.rand(opt.batch_size, n_memory_vectors + 2, opt.memory_size))
+        table.insert(grads, torch.rand(opt.batch_size, n_memory_vectors + 2))
+        table.insert(grads, torch.rand(opt.batch_size, opt.memory_size))
+    end
+    table.insert(grads, torch.rand(opt.batch_size, opt.vocab_size))
 
-
-print('\nDEQUE CONTROLLER')
-rnn = Controller.DeQue(opt)
-
-inputs = {}
-x = torch.rand(opt.batch_size):mul(opt.vocab_size):add(1):floor()
-table.insert(inputs, x)
-for i = 1, opt.num_lstm_layers do
-    table.insert(inputs, torch.rand(opt.batch_size, opt.rnn_size))
-    table.insert(inputs, torch.rand(opt.batch_size, opt.rnn_size))
+    local gradient = rnn2:backward(inputs2, grads)
+    grads = {}
+    for i = 2, #gradient do 
+        table.insert(grads, gradient[i])
+    end
+    table.insert(grads, torch.rand(opt.batch_size, opt.vocab_size))
+    rnn1:backward(inputs1, grads)
 end
-for i = 1, opt.num_memory_modules do
-    table.insert(inputs, torch.rand(opt.batch_size, n_memory_vectors, opt.memory_size))
-    table.insert(inputs, torch.rand(opt.batch_size, n_memory_vectors))
-    table.insert(inputs, torch.rand(opt.batch_size, opt.memory_size))
-    table.insert(inputs, torch.rand(opt.batch_size, opt.memory_size))
-end
-print('\nForwards pass!')
-print(unpack(rnn:forward(inputs)))
 
-grads = {}
-for i = 1, opt.num_lstm_layers do
-    table.insert(grads, torch.rand(opt.batch_size, opt.rnn_size))
-    table.insert(grads, torch.rand(opt.batch_size, opt.rnn_size))
-end
-for i = 1, opt.num_memory_modules do
-    table.insert(grads, torch.rand(opt.batch_size, n_memory_vectors + 2, opt.memory_size))
-    table.insert(grads, torch.rand(opt.batch_size, n_memory_vectors + 2))
-    table.insert(grads, torch.rand(opt.batch_size, opt.memory_size))
-    table.insert(grads, torch.rand(opt.batch_size, opt.memory_size))
-end
-table.insert(grads, torch.rand(opt.batch_size, opt.vocab_size))
+function tests.testDeQue()
+    local rnn1 = Controller.DeQue(opt)
+    local rnn2 = Controller.DeQue(opt)
 
-print('\n\nBackwards pass!')
-gradient = rnn:backward(inputs, grads)
-print(unpack(gradient))
-rnn:updateParameters(1)
-print(#inputs)
-print(#gradient)
+    local inputs1 = {}
+    x = torch.rand(opt.batch_size):mul(opt.vocab_size):add(1):floor()
+    table.insert(inputs1, x)
+    for i = 1, opt.num_lstm_layers do
+        table.insert(inputs1, torch.rand(opt.batch_size, opt.rnn_size))
+        table.insert(inputs1, torch.rand(opt.batch_size, opt.rnn_size))
+    end
+    for i = 1, opt.num_memory_modules do
+        table.insert(inputs1, torch.rand(opt.batch_size, n_memory_vectors, opt.memory_size))
+        table.insert(inputs1, torch.rand(opt.batch_size, n_memory_vectors))
+        table.insert(inputs1, torch.rand(opt.batch_size, opt.memory_size))
+        table.insert(inputs1, torch.rand(opt.batch_size, opt.memory_size))
+    end
+    local output = rnn1:forward(inputs1)
+
+    local inputs2 = {}
+    table.insert(inputs2, x)
+    for i = 1, #output - 1 do
+        table.insert(inputs2, output[i])
+    end
+    rnn2:forward(inputs2)
+
+    local grads = {}
+    for i = 1, opt.num_lstm_layers do
+        table.insert(grads, torch.rand(opt.batch_size, opt.rnn_size))
+        table.insert(grads, torch.rand(opt.batch_size, opt.rnn_size))
+    end
+    for i = 1, opt.num_memory_modules do
+        table.insert(grads, torch.rand(opt.batch_size, n_memory_vectors + 4, opt.memory_size))
+        table.insert(grads, torch.rand(opt.batch_size, n_memory_vectors + 4))
+        table.insert(grads, torch.rand(opt.batch_size, opt.memory_size))
+        table.insert(grads, torch.rand(opt.batch_size, opt.memory_size))
+    end
+    table.insert(grads, torch.rand(opt.batch_size, opt.vocab_size))
+
+    local gradient = rnn2:backward(inputs, grads)
+    grads = {}
+    for i = 2, #gradient do 
+        table.insert(grads, gradient[i])
+    end
+    table.insert(grads, torch.rand(opt.batch_size, opt.vocab_size))
+    rnn1:backward(inputs1, grads)
+end
+
+tester:add(tests)
+tester:run()
