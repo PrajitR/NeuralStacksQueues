6ef88189ccd5f7120fa9919b27cb87fe75222878
Prajit Ramachandran <ramprajit@gmail.com>
2015-09-06 14:36:36 -0500

    Better tests for controller.

--- test/controllerTest.lua
+++ test/controllerTest.lua
@@ -31,13 +31,26 @@ function tests.testOneSidedMemory()
         table.insert(inputs1, torch.rand(opt.batch_size, opt.memory_size))
     end
     local output = rnn1:forward(inputs1)
+    tester:asserteq(#output, 2  * opt.num_lstm_layers + 3 * opt.num_memory_modules + 1,
+                    "Length of outputs in rnn1 forward pass is wrong!")
 
     local inputs2 = {}
     table.insert(inputs2, x)
     for i = 1, #output - 1 do
         table.insert(inputs2, output[i])
     end
-    rnn2:forward(inputs2)
+    local output2 = rnn2:forward(inputs2)
+    tester:asserteq(#output2, 2  * opt.num_lstm_layers + 3 * opt.num_memory_modules + 1,
+                    "Length of outputs in rnn2 forward pass is wrong!")
+
+    local cumsum_reverse = { [true] = 0, [false] = 0 }
+    rnn1:apply(function(m) 
+        if torch.typename(m) == "nn.CumulativeSum" then
+            cumsum_reverse[m.reverse] = cumsum_reverse[m.reverse] + 1
+        end
+    end)
+    tester:asserteq(cumsum_reverse[true], 4, "Number of Stack memory modules is wrong!")
+    tester:asserteq(cumsum_reverse[false], 2, "Number of Queue memory modules is wrong!")
 
     local grads = {}
     for i = 1, opt.num_lstm_layers do
@@ -50,14 +63,16 @@ function tests.testOneSidedMemory()
         table.insert(grads, torch.rand(opt.batch_size, opt.memory_size))
     end
     table.insert(grads, torch.rand(opt.batch_size, opt.vocab_size))
-
     local gradient = rnn2:backward(inputs2, grads)
+    tester:asserteq(#gradient, #inputs2, "Length of gradients in rnn2 backwards pass is wrong!")
+
     grads = {}
     for i = 2, #gradient do 
         table.insert(grads, gradient[i])
     end
     table.insert(grads, torch.rand(opt.batch_size, opt.vocab_size))
-    rnn1:backward(inputs1, grads)
+    gradient = rnn1:backward(inputs1, grads)
+    tester:asserteq(#gradient, #inputs1, "Length of gradients in rnn1 backwards pass is wrong!")
 end
 
 function tests.testDeQue()
@@ -78,13 +93,17 @@ function tests.testDeQue()
         table.insert(inputs1, torch.rand(opt.batch_size, opt.memory_size))
     end
     local output = rnn1:forward(inputs1)
+    tester:asserteq(#output, 2  * opt.num_lstm_layers + 4 * opt.num_memory_modules + 1,
+                    "Length of outputs in rnn1 forward pass is wrong!")
 
     local inputs2 = {}
     table.insert(inputs2, x)
     for i = 1, #output - 1 do
         table.insert(inputs2, output[i])
     end
-    rnn2:forward(inputs2)
+    local output2 = rnn2:forward(inputs2)
+    tester:asserteq(#output2, 2  * opt.num_lstm_layers + 4 * opt.num_memory_modules + 1,
+                    "Length of outputs in rnn2 forward pass is wrong!")
 
     local grads = {}
     for i = 1, opt.num_lstm_layers do
@@ -98,14 +117,16 @@ function tests.testDeQue()
         table.insert(grads, torch.rand(opt.batch_size, opt.memory_size))
     end
     table.insert(grads, torch.rand(opt.batch_size, opt.vocab_size))
-
     local gradient = rnn2:backward(inputs, grads)
+    tester:asserteq(#gradient, #inputs2, "Length of gradients in rnn2 backwards pass is wrong!")
+
     grads = {}
     for i = 2, #gradient do 
         table.insert(grads, gradient[i])
     end
     table.insert(grads, torch.rand(opt.batch_size, opt.vocab_size))
-    rnn1:backward(inputs1, grads)
+    gradient = rnn1:backward(inputs1, grads)
+    tester:asserteq(#gradient, #inputs1, "Length of gradients in rnn1 backwards pass is wrong!")
 end
 
 tester:add(tests)
